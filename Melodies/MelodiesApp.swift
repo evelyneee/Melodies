80bc9ad52a0ff009601a51a67dcde314fe90437a
althiometer <charlesdelorean1@icloud.com>
2021-01-09 17:35:56 -0500

    Initial Commit

---
+++ Melodies/MelodiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodiesApp.swift
+//  Melodies
+//
+//  Created by Charles Bélanger on 2021-01-09.
+//
+
+import SwiftUI
+
+@main
+struct MelodiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
